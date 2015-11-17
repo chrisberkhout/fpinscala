0c69b3f9db46f14a86d13a9aaa488dc7969056a2
Chris Berkhout <chrisberkhout@gmail.com>
2015-11-17 22:28:03 +0100

    Started chapter 4.

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -4,15 +4,23 @@ package fpinscala.errorhandling
 import scala.{Option => _, Some => _, Either => _, _} // hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
 
 sealed trait Option[+A] {
-  def map[B](f: A => B): Option[B] = sys.error("todo")
+  def map[B](f: A => B): Option[B] = this match {
+    case None => None
+    case Some(x) => Some(f(x))
+  }
 
-  def getOrElse[B>:A](default: => B): B = sys.error("todo")
+  def getOrElse[B>:A](default: => B): B = this match {
+    case None => default
+    case Some(x) => x
+  }
 
-  def flatMap[B](f: A => Option[B]): Option[B] = sys.error("todo")
+  def flatMap[B](f: A => Option[B]): Option[B] = this.map(f).getOrElse(None)
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] = sys.error("todo")
+  def orElse[B>:A](ob: => Option[B]): Option[B] = this.map(Some(_)).getOrElse(ob)
 
-  def filter(f: A => Boolean): Option[A] = sys.error("todo")
+  def filter(f: A => Boolean): Option[A] =
+    if (this.map(f).getOrElse(false)) this
+    else None
 }
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
@@ -38,11 +46,22 @@ object Option {
   def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
-  def variance(xs: Seq[Double]): Option[Double] = sys.error("todo")
 
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = sys.error("todo")
+  def variance(xs: Seq[Double]): Option[Double] =
+//    mean(xs).map(m => xs.map(x => math.pow(x - m, 2))).flatMap(mean) // mine
+//    mean(xs) flatMap (m => mean(xs.map(x => math.pow(x - m, 2)))) // answer
+    mean(xs).flatMap(m => mean(xs.map(x => math.pow(x - m, 2)))) // mine rewritten == answer
 
-  def sequence[A](a: List[Option[A]]): Option[List[A]] = sys.error("todo")
+  // up to 4.3.2
 
-  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = sys.error("todo")
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = sys.error("todo ex 4.3")
+
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = sys.error("todo ex 4.4")
+
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = sys.error("todo ex 4.5")
+
+  // ex 4.6
+  // ex 4.7
+  // ex 4.8
+
 }
